8531bb75ec4e00a507ed051199495f7f7a10a622
Isaac Ross <isaacross@Isaacs-MBP.fios-router.home>
2015-06-05 20:23:52 -0400

    refactoring tests and making validator configurable

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -1,53 +1,51 @@
 /**
  * Created by Isaac on 6/5/2015.
  */
-var assert = require('assert'),
-    chai = require('chai'),
+var chai = require('chai'),
     expect = chai.expect,
-    validator = require('../lib/validator.js');
-
-
+    validatorWith = require('../lib/validator'),
+    nonPositiveValidationRule = require('../lib/rules/nonPositive'),
+    nonDivisibleValidationRule = require('../lib/rules/nonDivisible');
 
 describe('A validator', function () {
-    it('will return no errors for valid numbers', function(){
-        expect(validator(7)).to.be.empty;
-    });
-
-    describe('will include error.nonpositive for not strictly positive numbers', function () {
-        it('like 0', function(){
-            expect(validator(0)).to.include('error.nonpositive')
+    var validator;
+    context('with the default validation rules', function(){
+        beforeEach(function(){
+            validator = validatorWith([
+                nonPositiveValidationRule,
+                nonDivisibleValidationRule(3, 'error.three'),
+                nonDivisibleValidationRule(5, 'error.five')
+            ]);
         });
 
-        it('like -2', function(){
-            expect(validator(-2)).to.include('error.nonpositive');
-        });
-    });
-
-    describe('will include error.three for divisible by 3 numbers', function () {
-        it('like 3', function(){
-            expect(validator(3)).to.include('error.three');
+        it('will return no errors for valid numbers', function(){
+            expect(validator(7)).to.be.empty;
         });
 
-        it('like 6', function(){
-            expect(validator(6)).to.include('error.three');
+        describe('will include error.nonpositive for not strictly positive numbers', function () {
+            willInclude('error.nonpositive', 0);
+            willInclude('error.nonpositive', -2);
         });
 
-        it('like 15', function(){
-            expect(validator(15)).to.include('error.three');
-        })
-    });
+        describe('will include error.three for divisible by 3 numbers', function () {
+            willInclude('error.three', 3);
+            willInclude('error.three', 6);
+            willInclude('error.three', 15);
 
-    describe('will include error.five for divisible by 5 numbers', function () {
-        it('like 5', function(){
-           expect(validator(5)).to.include('error.five');
         });
 
-        it('like 10', function () {
-            expect(validator(10)).to.include('error.five');
-        });
-
-        it('like 15', function () {
-            expect(validator(15)).to.include('error.five');
+        describe('will include error.five for divisible by 5 numbers', function () {
+            willInclude('error.five', 5);
+            willInclude('error.five', 10);
+            willInclude('error.five', 15);
         });
     });
 });
+
+
+/// ------ Helpers
+function willInclude(error, num){
+    it('like ' + num, function(){
+        expect(validator(num)).to.include(error);
+    })
+}
